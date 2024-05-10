8295dc9a8e8cb40efd3978425c289befd408b6ce
Luke Graham <luke.evan.graham@gmail.com>
2024-05-10 16:57:33 -0700

    adding programs

--- app/page.js
+++ app/page.js
@@ -47,27 +47,94 @@ export default async function Home() {
         {homeData.data.attributes.Sections.map((section, index) => (
           <Sections sectionData={section} key={index} />
         ))}
-        {/* <div
-          style={{
-            backgroundColor: "#2B523B",
-            height: "50rem",
-            color: "#eee",
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <div>
-            <video
-              src={
-                "https://player.vimeo.com/progressive_redirect/playback/940916372/rendition/360p/file.mp4?loc=external&log_user=0&signature=f23ad4cb7d86ba61edeea9ee0e3fb22461a4a7f7f8241f0c445533085dde266f"
-              }
-              width={600}
-              height={300}
-              controls
-            />
+        <section className={`u-padding-y-large ${classes.Programs}`}>
+          <div className="row">
+            <h2>Our Programs</h2>
+
+            <div className={classes.Programs__Group}>
+              <div className={classes.Programs__Group__Program}>
+                <div className={classes.Programs__Group__Program__Top}>
+                  <div className={classes.Programs__Group__Program__Top__Image}>
+                    <Image
+                      src={`https://res.cloudinary.com/dzekujbym/image/upload/v1714594173/costume_6fc5fb6730.jpg?updated_at=2024-05-02T05:26:44.957Z`}
+                      alt="Test text"
+                      fill
+                    />
+                  </div>
+                  <div className={classes.Programs__Group__Program__Top__Text}>
+                    <h5>
+                      Parent & Child <br /> Classes
+                    </h5>
+                  </div>
+                </div>
+                <div className={classes.Programs__Group__Program__Body}>
+                  <div
+                    className={classes.Programs__Group__Program__Body__TopInfo}
+                  >
+                    For children 1-3 years of age. Designed around parent and
+                    child learning together.
+                  </div>
+                  <div className={classes.Programs__Group__Program__Body__List}>
+                    <div>Classes for 12-24 months</div>
+                    <div>Classes for 24-36 months</div>
+                  </div>
+                </div>
+              </div>
+              <div className={classes.Programs__Group__Program}>
+                <div className={classes.Programs__Group__Program__Top}>
+                  <div className={classes.Programs__Group__Program__Top__Image}>
+                    <Image
+                      src={`https://res.cloudinary.com/dzekujbym/image/upload/v1715369436/vets_84edf5977f.jpg?updated_at=2024-05-10T19:30:36.840Z`}
+                      alt="Test text"
+                      fill
+                    />
+                  </div>
+                  <div className={classes.Programs__Group__Program__Top__Text}>
+                    <h5>Preschool</h5>
+                  </div>
+                </div>
+                <div className={classes.Programs__Group__Program__Body}>
+                  <div
+                    className={classes.Programs__Group__Program__Body__TopInfo}
+                  >
+                    Age is determined by your child's age on September 1, {new Date().getFullYear()}. 
+                  </div>
+                  <div className={classes.Programs__Group__Program__Body__List}>
+                    <div>Classes for 2 year olds</div>
+                    <div>Classes for 3 year olds</div>
+                    <div>Classes for 4/5 year olds</div>
+                  </div>
+                </div>
+              </div>
+              <div className={classes.Programs__Group__Program}>
+                <div className={classes.Programs__Group__Program__Top}>
+                  <div className={classes.Programs__Group__Program__Top__Image}>
+                    <Image
+                      src={`https://res.cloudinary.com/dzekujbym/image/upload/v1715369436/shopping_d1f4a6dcab.jpg?updated_at=2024-05-10T19:30:36.821Z`}
+                      alt="Test text"
+                      fill
+                    />
+                  </div>
+                  <div className={classes.Programs__Group__Program__Top__Text}>
+                    <h5>Options</h5>
+                  </div>
+                </div>
+                <div className={classes.Programs__Group__Program__Body}>
+                  <div
+                    className={classes.Programs__Group__Program__Body__TopInfo}
+                  >
+                    There are different ways to extend your enrolled 3 and 4/5 year old's time with us.
+                  </div>
+                  <div className={classes.Programs__Group__Program__Body__List}>
+                    <div>Extended Day (6 hr classes)</div>
+                    <div>Lunch Bunch</div>
+                    <div>Early Drop Off</div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <p>We want the best for our kids.</p>
-        </div> */}
+        </section>
       </main>
     </Layout>
   );
