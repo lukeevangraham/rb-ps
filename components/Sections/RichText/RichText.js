3887948d02ed9d3f6094fc611bb68848f1b2c0f8
Luke Graham <luke.evan.graham@gmail.com>
2024-06-03 22:21:15 -0700

    about page progress

--- components/Sections/RichText/RichText.js
+++ components/Sections/RichText/RichText.js
@@ -3,7 +3,7 @@ import classes from "./RichText.module.scss";
 const RichText = ({ data }) => (
   <section className={`u-padding-y-large ${classes.RichText}`}>
     <div className="row">
-      <div dangerouslySetInnerHTML={{ __html: data.RichText }}>{console.log("D: ", data)}</div>
+      <div dangerouslySetInnerHTML={{ __html: data.RichText }}></div>
     </div>
   </section>
 );
