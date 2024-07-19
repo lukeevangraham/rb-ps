58197f111bd76d86bb49347ef40a318b205e0a51
Luke Graham <luke.evan.graham@gmail.com>
2024-07-19 12:01:58 -0700

    adding a custom 404

---
+++ app/not-found.js
@@ -0,0 +1,22 @@
+import Layout from "@/components/UI/Layout/Layout";
+import { getGlobalInfo } from "@/lib/api";
+
+const getData = async () => {
+  const res = await Promise.all([getGlobalInfo()]);
+
+  return res;
+};
+
+const NotFound = async () => {
+  const [globalData] = await getData();
+
+  return (
+    <Layout global={globalData.data.attributes}>
+      <main className="row u-padding-y-large">
+        <h1>We can't find this page</h1>
+      </main>
+    </Layout>
+  );
+};
+
+export default NotFound;
