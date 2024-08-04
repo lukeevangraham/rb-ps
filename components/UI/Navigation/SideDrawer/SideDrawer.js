b84cffb296b86b5e1268708c51f86b2c13c81b5e
Luke Graham <luke.evan.graham@gmail.com>
2024-08-04 12:30:31 -0700

    side drawer with links

--- components/UI/Navigation/SideDrawer/SideDrawer.js
+++ components/UI/Navigation/SideDrawer/SideDrawer.js
@@ -1,4 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
 import Backdrop from "../../Backdrop/Backdrop";
+import Brandname from "../../Brandname/Brandname";
 
 import classes from "./SideDrawer.module.scss";
 
@@ -10,7 +13,30 @@ const SideDrawer = ({ open, closed, global }) => (
         open ? classes.SideDrawer__open : classes.SideDrawer__closed
       }`}
     >
-      SideDrawer
+      <Link href={`/`}>
+        <div className={classes.SideDrawer__Brand}>
+          <div className={classes.SideDrawer__Brand__Logo}>
+            <Image
+              src={global.Navbar.Logo.data.attributes.url}
+              alt={global.Navbar.Logo.data.attributes.alternativeText}
+              fill
+            />
+          </div>
+          <div className={classes.SideDrawer__Brand__Brandname}>
+            <Brandname />
+          </div>
+        </div>
+      </Link>
+      <div className={classes.SideDrawer__Nav}>
+        <ul>
+          {global.Navbar.Links.map((link) => (
+            <li>
+              <Link href={`.${link.url}`}>{link.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* {console.log("SD G: ", global.Navbar)} */}
     </div>
   </>
 );
