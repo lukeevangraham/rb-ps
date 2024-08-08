bf0093d5322f74d34c388868083bd6f5c0c3f8c5
Luke Graham <luke.evan.graham@gmail.com>
2024-08-08 15:37:02 -0700

    megamenu working in mobile

--- components/UI/Navigation/Navigation Items/Navigation Items.js
+++ components/UI/Navigation/Navigation Items/Navigation Items.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import NavigationItem from "./Navigation Item/Navigation Item";
 import Link from "next/link";
 
@@ -9,39 +9,47 @@ import classes from "./Navigation Items.module.scss";
 const NavigationItems = ({ links, button }) => {
   const [parentsClicked, setParentsClicked] = useState(false);
 
+  const parentMenu = (source) => (
+    <div className={source == "toolbar" ? classes.DesktopOnly : classes.MobileOnly}>
+      <div className={`${classes.Nav__MegaMenu} ${classes.Nav__MegaMenu__on}`}>
+        <li>
+          <Link href={`/parents/handbook`}>Parent Handbook</Link>
+        </li>
+        <li>
+          <Link href={`/parents/calendar`}>Calendar & Dates</Link>
+        </li>
+        <li>
+          <Link href={`/parents/registration`}>Registration Packets</Link>
+        </li>
+        <li>
+          <Link href={`/parents/resources`}>Helpful Resources</Link>
+        </li>
+      </div>
+    </div>
+  );
+
   return (
     <nav className={classes.Nav}>
       <ul className={classes.Nav__List}>
         {links.map((link) => (
-          <NavigationItem
-            key={link.id}
-            item={link}
-            parentsClicked={parentsClicked}
-            setParentsClicked={setParentsClicked}
-          />
+          <React.Fragment key={link.id}>
+            <NavigationItem
+              item={link}
+              parentsClicked={parentsClicked}
+              setParentsClicked={setParentsClicked}
+            />
+            {parentsClicked && link.url == "/parents"
+              ? parentMenu("sideDrawer")
+              : null}
+          </React.Fragment>
         ))}
       </ul>
 
       {parentsClicked ? (
-        <div
-          className={`${classes.Nav__MegaMenu} ${classes.Nav__MegaMenu__on}`}
-        >
-          <div>
-            <Link href={`/parents/handbook`}>Parent Handbook</Link>
-          </div>
-          <div>
-            <Link href={`/parents/calendar`}>Calendar & Dates</Link>
-          </div>
-          <div>
-            <Link href={`/parents/registration`}>Registration Packets</Link>
-          </div>
-          <div>
-            <Link href={`/parents/resources`}>Helpful Resources</Link>
-          </div>
-        </div>
+        parentMenu("toolbar")
       ) : (
         <div
-          className={`${classes.Nav__MegaMenu} ${classes.Nav__MegaMenu__off}`}
+          className={`${classes.Nav__MegaMenu} ${classes.Nav__MegaMenu__off}}`}
         ></div>
       )}
     </nav>
