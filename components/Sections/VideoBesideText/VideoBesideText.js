4f3ff263218c00e9d5128f168344ba9d79d1b281
Luke Graham <luke.evan.graham@gmail.com>
2024-10-04 15:31:35 -0700

    working on the play icon, but it's an eye currently

--- components/Sections/VideoBesideText/VideoBesideText.js
+++ components/Sections/VideoBesideText/VideoBesideText.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import PlayIcon from "../../../public/images/play.svg";
+
 import classes from "./VideoBesideText.module.scss";
 
 const VideoBesideText = ({ data }) => (
@@ -8,14 +10,15 @@ const VideoBesideText = ({ data }) => (
     <div className="row">
       <div className={classes.VideoBesideText}>
         <div className={classes.VideoBesideText__Video}>
-          <svg>
-            <use xlinkHref="../images/sprite.svg#icon-play" />
-          </svg>
-          <div className={classes.VideoBesideText__Video__Frame}>
+          {/* <div className={classes.VideoBesideText__Video__Frame}> */}
+            <svg>
+              <use xlinkHref="../images/sprite.svg#icon-play" />
+            </svg>
+            <Image src={PlayIcon} className={classes.VideoBesideText__Video__Frame} />
             <Link href={`/video?url=${data.videoURL}}`}>
               <Image src={data.videoURL.replace(".mp4", ".jpg")} fill />
             </Link>
-          </div>
+          {/* </div> */}
         </div>
         <div
           className={classes.VideoBesideText__Text}
