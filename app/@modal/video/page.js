72811f830d331eecc67cf099c87618619a9013cc
Luke Graham <luke.evan.graham@gmail.com>
2025-04-01 16:31:32 -0700

    adding a suspense to a video modal

--- app/@modal/video/page.js
+++ app/@modal/video/page.js
@@ -3,21 +3,34 @@
 import ModalNew from "@/components/UI/ModalNew/ModalNew";
 // import { useRouter } from "next/router";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 import classes from "./page.module.scss";
 
-const VideoModal = () => {
+function Video() {
   const searchParams = useSearchParams();
   const videoURL = searchParams.get("url");
 
   return (
-    <>
+    <div className={classes.Video}>
+      <video src={videoURL} controls autoPlay />
+    </div>
+  );
+}
+
+const VideoModal = () => {
+  // const searchParams = useSearchParams();
+  // const videoURL = searchParams.get("url");
+
+  return (
+    <Suspense>
       <ModalNew>
-        <div className={classes.Video}>
+        {/* <div className={classes.Video}>
           <video src={videoURL} controls autoPlay />
-        </div>
+        </div> */}
+        <Video />
       </ModalNew>
-    </>
+    </Suspense>
   );
 };
 
