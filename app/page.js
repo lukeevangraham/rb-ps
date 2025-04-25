9b49acf631b088610de53bbd2f088f4002e31796
Luke Graham <luke.evan.graham@gmail.com>
2025-04-25 16:49:06 -0700

    better og url

--- app/page.js
+++ app/page.js
@@ -49,7 +49,7 @@ export const metadata = {
     url: "/",
     images: [
       {
-        url: "../images/rbcpcPreschoolOG.jpg",
+        url: "https://res.cloudinary.com/dzekujbym/image/upload/v1745624911/rbcpc_Prechool_OG_6efb2b8eec.jpg?updated_at=2025-04-25T23:48:31.780Z",
         width: 1200,
         height: 630,
         alt: "RBCPC Preschool",
